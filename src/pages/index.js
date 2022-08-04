5b63ea5dec77c59b54e8eadbb9f6f93f70a07403
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-08-04 20:58:06 +0700

    fix: styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,19 @@ import Image from 'next/image';
 const EVENTS = [
 	{
 		title: 'NAO',
-		subtitle: 'Lorem ipsum dolor',
-		body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In iste tenetur doloribus distinctio voluptatibus, corrupti illo, facere beatae error ad.',
+		subtitle: 'National Accounting Olympiad',
+		body: 'National Accounting Olympiad (NAO) is one of the largest and most prestigious Accounting Olympiads in Indonesia. As part of the Gadjah Mada Accounting Days (GMAD) 2022, NAO is here to prove that an olympiad can provide solutions for current accounting issues, packed in the form of a case study and executed with a great deal of fun yet also challenging.',
 		imageUrl: '/images/homepage/event-nao.png',
 		imageAlt: 'NAO',
 		buttonText: 'learn more',
 		buttonPushTo: '/nao'
 	},
 	{
-		title: 'Atalks',
-		subtitle: 'Lorem ipsum dolor',
-		body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In iste tenetur doloribus distinctio voluptatibus, corrupti illo, facere beatae error ad.',
+		title: 'Atalk',
+		subtitle: 'Accounting Talk',
+		body: 'Accounting Talk (A-Talk) is the biggest accounting seminar intended for students, business, people, civil servants, and the general public. In an era full of uncertainty, A-Talk advances to discuss optimization and the application of digitalization to the business in the future.',
 		imageUrl: '/images/homepage/event-atalks.png',
-		imageAlt: 'Atalks',
+		imageAlt: 'Atalk',
 		buttonText: 'learn more',
 		buttonPushTo: '/atalks'
 	}
@@ -39,22 +39,21 @@ export default function Home() {
 	return (
 		<DefaultLayout title="GMAD 2022">
 			{/* section 1: gmad */}
-			<section className="grid place-content-center min-h-screen bg-green-dark text-p1">
-				<div className="flex flex-col-reverse md:flex-row justify-center container w-full outline items-center md:space-y-0 md:space-x-8">
+			<section className="grid place-content-center min-h-screen bg-[url('/images/bg/bg-homepage-1.png')] bg-cover bg-center">
+				<div className="flex flex-col-reverse md:flex-row justify-center container w-full items-center md:space-y-0 md:space-x-8">
 					<div
 						className="md:w-[70%] lg:w-[60%] space-y-6"
 						data-aos="fade-right"
 						data-aos-delay="3000"
 					>
-						<h1 className="font-chromate text-c-yellow text-h3 lg:text-h1">
-							Lorem Ipsum
+						<h1 className="font-chromate text-c-yellow outlineText">
+							Gadjah Mada Accounting Days 2022
 						</h1>
-						<div className=" text-white font-medium text-justify">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-							doloribus magni optio facere nemo consequatur, eaque inventore
-							quos eveniet animi, perspiciatis itaque. Ratione rem facilis modi
-							qui quas explicabo sapiente.
-						</div>
+						<p className=" text-white font-medium text-justify">
+							The biggest annual events of the Department of Accounting, Faculty
+							of Economics and Business, Universitas Gadjah Mada (FEB UGM),
+							organized by Ikatan Mahasiswa Akuntansi Gadjah Mada (IMAGAMA).
+						</p>
 					</div>
 					<div className="max-w-[40vh] mb-8 md:mb-0">
 						<AnimeLogo />
@@ -74,47 +73,44 @@ export default function Home() {
 			</section>
 
 			{/* section 2: grand theme */}
-			<section className="py-24 bg-green-light text-p1" data-aos="fade-right">
-				<div className="container outline mx-auto">
-					<h1 className="font-chromate text-c-yellow text-h3 lg:text-h1 text-center -my-6">
+			<section className="py-24 text-p1 bg-green-light shadow-2xl">
+				<div className="container mx-auto" data-aos="fade-up">
+					<h1 className="font-chromate text-c-yellow text-center pb-8 outlineText2">
 						Grand Theme
 					</h1>
-					<div className="mt-2 text-white font-medium text-center">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					</div>
-					<div className="border-[3px] border-yellow-500 mt-8 rounded-2xl bg-gradient-to-br from-[rgba(255,255,255,0.2)] to-green-light p-8 relative">
-						<div className="flex">
-							<div className="w-0 lg:w-[45%]"></div>
-							<div className="w-full lg:w-[55%]">
-								<h2 className="text-h2 font-bold text-center">Heading 2</h2>
-								<div className="text-h3 font-bold text-c-yellow text-center">
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					<div className="border-[3px] outline-gradient mt-8 rounded-2xl bg-gradient-to-br from-[rgba(255,255,255,0.2)] to-green-light relative">
+						<div className="grid grid-cols-1 lg:grid-cols-2 p-12">
+							<div className="w-full aspect-video mx-auto rounded-3xl overflow-hidden">
+								<iframe
+									width="100%"
+									height="100%"
+									src="https://www.youtube.com/embed/SbCl6n6kQQA?start=1"
+									title="YouTube video player"
+									frameBorder="0"
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+									allowFullScreen
+								></iframe>
+							</div>
+							<div className="grid place-content-center">
+								<h2 className="text-xl lg:text-3xl font-bold text-center">
+									Advancement of Business Environment :
+								</h2>
+								<div className="text-2xl lg:text-4xl font-bold text-c-yellow text-center">
+									&ldquo;Facing off Uncertainty in Digitized Era&rdquo;
 								</div>
 							</div>
-						</div>
-						<div className="absolute outline bottom-0 lg:w-[450px] lg:h-[450px] hidden lg:block translate-y-[21.8%]">
-							<Image
-								src="/images/homepage/man-counting.png"
-								alt=""
-								width={550}
-								height={550}
-								objectFit="contain"
-							/>
 						</div>
 					</div>
 				</div>
 			</section>
 
 			{/* section 3: our main events */}
-			<section className="py-24 bg-green-light text-p1">
-				<div className="container outline mx-auto">
-					<h1 className="font-chromate text-c-yellow text-h3 lg:text-h1 text-center -my-6">
+			<section className="py-24 text-p1 bg-[url('/images/bg/bg-homepage-3.png')] bg-cover bg-center">
+				<div className="container mx-auto">
+					<h1 className="font-chromate text-c-yellow text-center outlineText2">
 						Our Main Events
 					</h1>
-					<div className="mt-2 text-white font-medium text-center">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					</div>
-					<div className="grid grid-cols-2 gap-12 mt-16 max-w-6xl mx-auto">
+					<div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mt-16 max-w-6xl mx-auto">
 						{EVENTS.map((event, index) => (
 							<MainEventCard
 								key={index}
@@ -132,17 +128,14 @@ export default function Home() {
 			</section>
 
 			{/* seciton 4: social media */}
-			<section className="py-24 bg-green-light text-p1">
-				<div className="container outline mx-auto">
-					<h1 className="font-chromate text-c-yellow text-h3 lg:text-h1 text-center -my-6">
+			<section className="py-24 text-p1 bg-[url('/images/bg/bg-homepage-4.png')] bg-cover bg-center">
+				<div className="container mx-auto">
+					<h1 className="font-chromate text-c-yellow text-center outlineText2">
 						Social Media
 					</h1>
-					<div className="mt-2 text-white font-medium text-center">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					</div>
-					<div className="grid grid-cols-2 gap-12 mt-16 max-w-3xl mx-auto">
+					<div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mt-16 max-w-3xl mx-auto w-full place-items-center">
 						{SOCIAL_MEDIAS.map((sosmed, index) => (
-							<div key={index} className="shadow-2xl">
+							<div key={index} className="drop-shadow-2xl">
 								<Image
 									src={sosmed.imageUrl}
 									alt={sosmed.name}
